2de1e04297e588b29f63eebe3ac831074a3aac34
mealdy <yingfeng.chu@gmail.com>
2015-07-28 01:02:03 +0800

    fix some links and text content bug

--- js/site.js
+++ js/site.js
@@ -2,3 +2,18 @@ var banner = document.getElementById('banner');
 setTimeout(function() {
 	banner.style.marginTop = '0';
 	}, 800);
+	
+$(document).ready(function(){
+	$('a[href^="#about"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 600, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
